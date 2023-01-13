bfdf9e771ae3471b7494d05836b92b306205481e
Neema Totonchi <ntotonchi1@se14l0120-15.ad.bcit.ca>
2023-01-13 13:07:55 -0800

    fixed counter page styling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1f091ac4ae3ba65"; }
+/******/ 		__webpack_require__.h = function() { return "fc04b0dc85c939f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
